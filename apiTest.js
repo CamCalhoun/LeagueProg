af63a2a24652aab770e9625f994523a741c93eeb
Cameron Calhoun <cameron.calhoun01@gmail.com>
2024-11-04 12:54:37 -0500

    made first call

--- apiTest.js
+++ apiTest.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+const dotenv = require('dotenv')
+dotenv.config()
+
+const API_KEY = process.env.API_KEY
+const gameName = "cammy1"
+const tagLine = "NA1"
+const uri = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${API_KEY}`
+fetch(uri).then((response) => {
+	return response.json()
+}).then((response) => {
+	console.log(response)
+})
+
